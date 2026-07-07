e209ede77771d6d37add7590f9cfc63d354d9f4b
caiomathiass <caiomathiass.cs27@gmail.com>
2026-07-06 21:15:29 -0300

    test

--- prog25.py
+++ prog25.py
@@ -1,6 +1,7 @@
 n1 = int(input("Coloque um valor"))
 n2 = int(input("Coloque outro valor"))
-if "n1" > "n2":
-    print(f" {n1}É maior que {n2} ")
-elif "n2" > "n1":
-    print(f"{n2}É maior que {n1}")
+if n1 > n2:
+    print(f" {n1} É maior que {n2} ")
+else:
+    print(f" {n2} É maior que {n1} ")
+print("teste de push")
